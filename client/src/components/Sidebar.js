bb2c6804feccd5518c2ec85b5434fb00c72ebb1c
EDowning2000 <emersondowning@gmail.com>
2020-08-19 17:23:14 -0400

    imports for the sidebar are finished

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,18 +1,24 @@
 import React from "react";
 import "./Sidebar.css";
-import TwitterIcon from '@material-ui/icons/Twitter';
-import SidebarOption from './SidebarOption'
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
+import SidebarOption from "./SidebarOption";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <TwitterIcon/>
-      {/* twitter icon
-    sidebar routing option component
-    sidebar routing option component
-    sidebar routing option component
-    sidebar routing option component */}
+      <TwitterIcon />
+      <SidebarOption Icon={HomeIcon} text="home" />
+      <SidebarOption Icon={SearchIcon} text="explore" />
+      <SidebarOption Icon={NotificationsNoneIcon} text="notifications" />
     </div>
   );
 }
--- client/src/components/SidebarOption.js
+++ client/src/components/SidebarOption.js
@@ -5,7 +5,8 @@ import './SidebarOption.css'
 function SidebarOption({text, Icon}){
   return (
     <div className="sidebarOption">
-
+      <Icon/>
+      <h3>{text}</h3>
     </div>
   )
 }
