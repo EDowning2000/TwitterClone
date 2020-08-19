81937335700bac956f93358d4819b2312dcbe840
EDowning2000 <emersondowning@gmail.com>
2020-08-19 16:29:36 -0400

    started the sidebar component and got it mapped out

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Sidebar.css'
+
+function Sidebar() {
+  return (
+  <div className="sidebar">
+    
+    {/* twitter icon
+
+    sidebar routing option component
+    sidebar routing option component
+    sidebar routing option component
+    sidebar routing option component */}
+
+
+  </div>
+  )
+}
+export default Sidebar;
