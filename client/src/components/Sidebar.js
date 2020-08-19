b485083caf6f74904ae4330de49eaadf29132fca
EDowning2000 <emersondowning@gmail.com>
2020-08-19 16:48:11 -0400

    added the material stuff and installed it for the icons and such

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,19 +1,17 @@
 import React from "react";
-import './Sidebar.css'
+import "./Sidebar.css";
+
 
 function Sidebar() {
   return (
-  <div className="sidebar">
-    
-    {/* twitter icon
+    <div className="sidebar">
+      {/* twitter icon
 
     sidebar routing option component
     sidebar routing option component
     sidebar routing option component
     sidebar routing option component */}
-
-
-  </div>
-  )
+    </div>
+  );
 }
 export default Sidebar;
