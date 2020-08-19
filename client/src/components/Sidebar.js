7e8b04912057ef248129499b4787adb3decd4e88
EDowning2000 <emersondowning@gmail.com>
2020-08-19 19:05:54 -0400

    side bar is finished

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -9,9 +9,9 @@ import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
 import ListAltIcon from "@material-ui/icons/ListAlt";
 import PermIdentityIcon from "@material-ui/icons/PermIdentity";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import { Button } from "@material-ui/core";
 
 import SidebarOption from "./SidebarOption";
-
 function Sidebar() {
   return (
     <div className="sidebar">
@@ -19,11 +19,12 @@ function Sidebar() {
       <SidebarOption active Icon={HomeIcon} text="Home" />
       <SidebarOption Icon={SearchIcon} text="Explore" />
       <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
-      <SidebarOption Icon={MailOutlineIcon} text="Messages"/>
-      <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks"/>
-      <SidebarOption Icon={ListAltIcon} text="Lists"/>
-      <SidebarOption Icon={PermIdentityIcon} text="Profile"/>
-      <SidebarOption Icon={MoreHorizIcon} text="More"/>
+      <SidebarOption Icon={MailOutlineIcon} text="Messages" />
+      <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
+      <SidebarOption Icon={ListAltIcon} text="Lists" />
+      <SidebarOption Icon={PermIdentityIcon} text="Profile" />
+      <SidebarOption Icon={MoreHorizIcon} text="More" />
+      <Button variant="outlined" className="tweetBtn" fullWidth>Tweet</Button>
     </div>
   );
 }
