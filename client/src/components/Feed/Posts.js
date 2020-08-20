88fc4f1e9d882631e4d9e43e5cae66b73b05658e
EDowning2000 <emersondowning@gmail.com>
2020-08-20 17:40:50 -0400

    signing off for a min

--- client/src/components/Feed/Posts.js
+++ client/src/components/Feed/Posts.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Posts.css";
 import { Avatar } from "@material-ui/core";
-import VerifiedUserIcon from "@material-ui/icons/VerifiedUserOutlined";
+import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
 import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
 import RepeatIcon from "@material-ui/icons/Repeat";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
@@ -13,6 +13,7 @@ function Posts({ name, userName, verified, tweetContent, image, avatar }) {
       <div className="postsAvatar">
         <Avatar src="https://pbs.twimg.com/profile_images/968882665188478978/3aqyFu3g_400x400.jpg" />
       </div>
+
       <div className="postsBody">
         <div className="postsHeader">
           <div className="postsHeaderText">
@@ -23,19 +24,26 @@ function Posts({ name, userName, verified, tweetContent, image, avatar }) {
               </span>
             </h3>
           </div>
+          {/* end of user info */}
+
           <div className="postsDescription">
             <p>Hello World</p>
           </div>
         </div>
-
-        <img src="https://media.giphy.com/media/lszAB3TzFtRaU/giphy.gif" />
+        {/* end of header */}
+        {/* eslint-disable-next-line */}
+        <img
+          src="https://media.giphy.com/media/lszAB3TzFtRaU/giphy.gif"
+          alt="tweet image"
+        />
+        {/* tweet image */}
         <div className="postsFooter">
-          <ChatBubbleOutlineIcon fontSize="small"/>
-          <FavoriteBorderIcon fontSize="small"/>
-          <RepeatIcon fontSize="small"/>
-          <PublishIcon fontSize="small"/>
-
+          <ChatBubbleOutlineIcon fontSize="small" />
+          <FavoriteBorderIcon fontSize="small" />
+          <RepeatIcon fontSize="small" />
+          <PublishIcon fontSize="small" />
         </div>
+        {/* end of post footer */}
       </div>
     </div>
   );
