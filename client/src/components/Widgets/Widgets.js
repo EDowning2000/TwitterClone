492aab99e351fb12a9f418b6ec70961a0cab159b
EDowning2000 <emersondowning@gmail.com>
2020-08-20 19:14:05 -0400

    working on embedding tweets

--- client/src/components/Widgets/Widgets.js
+++ client/src/components/Widgets/Widgets.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./Widgets.js";
+import SearchIcon from "@material-ui/icons/Search";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
 
 function Widgets() {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgetsInput">
+        <SearchIcon className="widgetsSearchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+      <div className="widgetsContainer">
+        <h2>What's Happening</h2>
+      </div>
     </div>
   );
 }
