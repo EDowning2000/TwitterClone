6554c054aeb1b07f745c42620b4fd5a8cdf30649
EDowning2000 <emersondowning@gmail.com>
2020-08-20 19:38:09 -0400

    working on the widgets function

--- client/src/components/Widgets/Widgets.js
+++ client/src/components/Widgets/Widgets.js
@@ -16,6 +16,21 @@ function Widgets() {
       </div>
       <div className="widgetsContainer">
         <h2>What's Happening</h2>
+
+        <TwitterTweetEmbed tweetId={"https://twitter.com/_thedevloper/status/1295971027042803714?cxt=HKwWhICtoYyym_wjAAAA"} />
+
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="EmersonDowning1"
+          options={{ height: 400 }}
+        />
+        <TwitterShareButton
+          url={"https://github.com/EDowning2000"}
+          options={{ text: "Emerson seems pretty good, i can find him here =>", via: "EmersonDowning1" }}
+        />
+
+
+
       </div>
     </div>
   );
