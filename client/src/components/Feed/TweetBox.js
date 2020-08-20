5bbab68221bce1ecd192bd78184a8d34587eac66
EDowning2000 <emersondowning@gmail.com>
2020-08-20 14:53:16 -0400

    got the input and the avatar for the tweet form all setup and looking nice

--- client/src/components/Feed/TweetBox.js
+++ client/src/components/Feed/TweetBox.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import './Tweetbox.css';
+import React from "react";
+import "./Tweetbox.css";
+import { Avatar, Button } from "@material-ui/core";
 
-function Tweetbox(){
+function Tweetbox() {
   return (
-    <div className='tweetbox'>
-
-
-
-
-
-      
+    <div className="tweetbox">
+      <form>
+        <div className="tweetboxInput">
+          <Avatar src="https://pbs.twimg.com/profile_images/968882665188478978/3aqyFu3g_400x400.jpg" />
+          <input placeholder="What's happening?" type="text"></input>
+          <input placeholder="Add image URL" type="text"></input>
+        </div>
+        <Button>Tweet</Button>
+      </form>
     </div>
-  )
+  );
 }
 export default Tweetbox;
