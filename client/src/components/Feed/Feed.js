cc08c0cf4d89edeae4b11ca35c6f4a8d09ba3a45
EDowning2000 <emersondowning@gmail.com>
2020-08-20 17:00:26 -0400

    imported a bunch of icons to the posts page and also got the layout ready

--- client/src/components/Feed/Feed.js
+++ client/src/components/Feed/Feed.js
@@ -1,20 +1,16 @@
-import React from 'react'
-import './Feed.css'
+import React from "react";
+import "./Feed.css";
 import Tweetbox from "./TweetBox";
+import Posts from "./Posts";
 
-function Feed (){
+function Feed() {
   return (
     <div className="feed">
-      <h2 className='header'>Home</h2>
-      <Tweetbox/>
+      <h2 className="header">Home</h2>
+      <Tweetbox />
+      <Posts/>
 
-
-
-
-
-
-
-
+      {/* <div>filler text</div>
       <div>filler text</div>
       <div>filler text</div>
       <div>filler text</div>
@@ -113,12 +109,8 @@ function Feed (){
       <div>filler text</div>
       <div>filler text</div>
       <div>filler text</div>
-      <div>filler text</div>
-      <div>filler text</div>
-
-      
-      
+      <div>filler text</div> */}
     </div>
-  )
+  );
 }
 export default Feed;
--- client/src/components/Feed/Posts.js
+++ client/src/components/Feed/Posts.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import './Post.css'
+import React from "react";
+import "./Posts.css";
+import { Avatar } from "@material-ui/core";
+import VerifiedUserIcon from "@material-ui/icons/VerifiedUserOutlined";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import PublishIcon from "@material-ui/icons/Publish";
 
-function Posts({
-  name, userName, verified
-}){
-  return(
-    <div className='posts'>
+function Posts({ name, userName, verified, tweetContent, image, avatar }) {
+  return (
+    <div className="posts">
+      <div className="postsAvatar">
+        <Avatar src="https://pbs.twimg.com/profile_images/968882665188478978/3aqyFu3g_400x400.jpg" />
+      </div>
+      <div className="postsBody">
+        <div className="postsHeader">
+          <div className="postsHeaderText">
+            <h3>
+              Emerson Downing{""}{" "}
+              <span>
+                <VerifiedUserIcon className="postsBadge" />
+              </span>
+            </h3>
+          </div>
+          <div className="postsDescription">
+            <p>Hello World</p>
+          </div>
+        </div>
 
+        <img src="https://media.giphy.com/media/lszAB3TzFtRaU/giphy.gif" />
+        <div className="postsFooter">
+          <ChatBubbleOutlineIcon fontSize="small"/>
+          <FavoriteBorderIcon fontSize="small"/>
+          <RepeatIcon fontSize="small"/>
+          <PublishIcon fontSize="small"/>
 
-
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-export default Posts
+export default Posts;
