d309701c932a01963bf4668589088b88a5407673
EDowning2000 <emersondowning@gmail.com>
2020-08-21 19:34:35 -0400

    connected firebase and got it exported

--- client/firebase.js
+++ client/firebase.js
@@ -1,3 +1,4 @@
+import firebase from "firebase";
 const firebaseConfig = {
   apiKey: "AIzaSyDnwTkJxPW13NacRWleqbJqjZ0gCKx2r1o",
   authDomain: "twitterclone-70948.firebaseapp.com",
@@ -6,5 +7,12 @@ const firebaseConfig = {
   storageBucket: "twitterclone-70948.appspot.com",
   messagingSenderId: "392610903600",
   appId: "1:392610903600:web:151ba87737284446309ff5",
-  measurementId: "G-2WZNYLH8N1"
+  measurementId: "G-2WZNYLH8N1",
 };
+//initializing the firebase app
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+//making this an easy variable to call in other files
+const db = firebaseApp.firestore();
+
+export default db;
