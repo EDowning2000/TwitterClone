ef4e0b148f6a61fa56794b4bacad390243970d4c
EDowning2000 <emersondowning@gmail.com>
2020-08-21 19:50:01 -0400

    state is setup in the feed file and imported

--- client/src/components/Feed/Feed.js
+++ client/src/components/Feed/Feed.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Feed.css";
 import Tweetbox from "./TweetBox";
 import Posts from "./Posts";
 
 function Feed() {
+  const [posts, setPosts] = useState([]);
+  //setting the initial state to an empty array
   return (
     <div className="feed">
       <h2 className="header">Home</h2>
       <Tweetbox />
-      <Posts name=""
-      userName=""
-      verified={true}
-      tweetContent=""
-      avatar=""
-      image="" />
+      <Posts
+        name=""
+        userName=""
+        verified={true}
+        tweetContent=""
+        avatar=""
+        image=""
+      />
       {/* <Posts/>
       <Posts/>
       <Posts/>
@@ -29,9 +33,6 @@ function Feed() {
       <Posts/>
       <Posts/>
       <Posts/> */}
-      
-
-      
     </div>
   );
 }
