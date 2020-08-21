f3b9504018fcf83acd5c10b67a4304043a572dc7
EDowning2000 <emersondowning@gmail.com>
2020-08-21 19:29:41 -0400

    firebase getting setup and added the firebase.js file in order to configure it

---
+++ client/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDnwTkJxPW13NacRWleqbJqjZ0gCKx2r1o",
+  authDomain: "twitterclone-70948.firebaseapp.com",
+  databaseURL: "https://twitterclone-70948.firebaseio.com",
+  projectId: "twitterclone-70948",
+  storageBucket: "twitterclone-70948.appspot.com",
+  messagingSenderId: "392610903600",
+  appId: "1:392610903600:web:151ba87737284446309ff5",
+  measurementId: "G-2WZNYLH8N1"
+};
