81b2940fa55ec1c8260a663568a617f2e44ea084
EDowning2000 <emersondowning@gmail.com>
2020-08-21 20:04:08 -0400

    getting off for dinner so im just saving in case of pulling down WIP

--- client/src/components/Feed/Feed.js
+++ client/src/components/Feed/Feed.js
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import "./Feed.css";
 import Tweetbox from "./TweetBox";
 import Posts from "./Posts";
+import db from '../../firebase'
 
 function Feed() {
   const [posts, setPosts] = useState([]);
   //setting the initial state to an empty array
+  useEffect(() => {
+    db.collection('posts').onSnapshot(snapshot=>{(
+      setPosts()
+    )})
+    
+  }, []);
+
   return (
     <div className="feed">
       <h2 className="header">Home</h2>
