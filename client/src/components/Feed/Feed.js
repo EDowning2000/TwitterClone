70602dec6fb4be15cbaab1172bb010526fca0020
EDowning2000 <emersondowning@gmail.com>
2020-08-22 19:45:17 -0400

    changed around the feed component and added the use effect

--- client/src/components/Feed/Feed.js
+++ client/src/components/Feed/Feed.js
@@ -1,31 +1,42 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./Feed.css";
 import Tweetbox from "./TweetBox";
 import Posts from "./Posts";
-import db from '../../firebase'
+import db from "../../firebase";
 
 function Feed() {
   const [posts, setPosts] = useState([]);
   //setting the initial state to an empty array
+
   useEffect(() => {
-    db.collection('posts').onSnapshot(snapshot=>{(
-      setPosts()
-    )})
-    
+    db.collection("posts").onSnapshot((snapshot) =>
+      setPosts(snapshot.docs.map((doc) => doc.data()))
+    );
   }, []);
+  //still a shade confused on this bit
 
   return (
     <div className="feed">
       <h2 className="header">Home</h2>
       <Tweetbox />
-      <Posts
+      {posts.map((post) => {
+        <Posts
+          name={post.name}
+          userName={post.userName}
+          verified={post.verified}
+          tweetContent={post.tweetContent}
+          avatar={post.avatar}
+          image={post.image}
+        />;
+      })}
+      {/* <Posts
         name=""
         userName=""
         verified={true}
         tweetContent=""
         avatar=""
         image=""
-      />
+      /> */}
       {/* <Posts/>
       <Posts/>
       <Posts/>
