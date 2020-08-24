4c2842d0a0aba433f6a9a5aff45b14f5410ddea2
EDowning2000 <emersondowning@gmail.com>
2020-08-23 22:41:40 -0400

    signing off for the evenining WIP

--- client/src/components/Feed/TweetBox.js
+++ client/src/components/Feed/TweetBox.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Tweetbox.css";
 import { Avatar, Button } from "@material-ui/core";
 
 function Tweetbox() {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+  const sendTweet = e
   return (
     <div className="tweetbox">
       <form>
         <div className="tweetboxInput">
           <Avatar src="https://pbs.twimg.com/profile_images/968882665188478978/3aqyFu3g_400x400.jpg" />
-          <input placeholder="What's happening?" type="text"></input>
-          
+          <input
+            onChange={(e) => setTweetMessage(e.target.value)}
+            value={tweetMessage}
+            placeholder="What's happening?"
+            type="text"
+          ></input>
         </div>
-        <input className="tweetboxInputImg" placeholder="Add image URL" type="text"></input>
-        <Button className='tweetboxBtn'>Tweet</Button>
+        <input
+          onChange={(e) => setTweetImage(e.target.value)}
+          value={tweetImage}
+          className="tweetboxInputImg"
+          placeholder="Add image URL"
+          type="text"
+        ></input>
+        <Button 
+        type="submit" 
+        className="tweetboxBtn"
+        onClick={sendTweet}>
+          Tweet
+        </Button>
       </form>
     </div>
   );
